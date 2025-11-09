5366164f8018e5f28d4987e7126299e1922b3302
ATEFALAMRY <rcoq12@gmail.com>
2025-11-09 18:43:42 +0300

    نص دن

--- HomePage/Homepage.js
+++ HomePage/Homepage.js
@@ -76,3 +76,42 @@ document.addEventListener("click", function (e) {
     closeLoginModal();
   }
 });
+
+let currentSlide = 0;
+let autoSlideInterval;
+
+const slides = document.querySelectorAll(".Notificaion-slide .slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+  resetAutoSlide();
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+  resetAutoSlide();
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(() => {
+    nextSlide();
+  }, 5000);
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval);
+  startAutoSlide();
+}
+
+window.addEventListener("load", () => {
+  showSlide(currentSlide);
+  startAutoSlide();
+});
